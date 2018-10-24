8f711aee5a8e4279077e6a71b89b3e84bc1dda91
ellakoo05 <ella@github.com>
2018-10-24 13:59:06 -0700

    changedUrl

--- index.js
+++ index.js
@@ -2,25 +2,62 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const rp = require('request-promise');
+const $ = require('cheerio');
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-app.get("/mysite", (req, resp)=>{
-    //resp.end("Hi welcome");
-    resp.json({
-        name:"Ella",
-        number:"5"
-    })
-})
+app.get("/fuelhubdata", (req, resp)=>{
+  //resp.end("Hi welcome");
+  let url = 'https://www.gasbuddy.com/home?lng=-123.12073750000002&lat=49.2827291&fuel=1&cursor=0';
+  var cursor = 0;
+  var prices = [];
+
+  function parseurl(){
+    url = 'https://www.gasbuddy.com/home?lng=-123.12073750000002&lat=49.2827291&fuel=1&cursor='+cursor;
+
+    rp(url).then(function(html){
+      //success!
+      //console.log(html);
+      var len = $('.styles__price___3DxO5', html).length,
+          pr = $('.styles__price___3DxO5', html),
+          addr = $('.styles__address___8IK98', html);
+
+      //console.log(len);
+      for(var i=0; i<len; i++){
+        prices.push({
+          price: pr[i].children[0].data,
+          addr:addr[i].children[0].data
+        });
+      }
+
+      if(cursor < 50){
+        cursor += 10;
+        parseurl();
+      } else {
+        console.log(prices);
+        //callback
+        resp.json(prices);
+      }
+    }).catch(function(err){
+      //handle error
+    });
+  }
+
+  parseurl();
+  
+  
+});
 
 app.listen(port, (err)=>{
-    if(err){
-        console.log(err);
-        return false; //whenever you want to kill the server
-    }
-    
-    console.log("Server started at port "+port);
+  if(err){
+    console.log(err);
+    return false;
+  }
+  
+  console.log("Server started at port "+port);
 })
