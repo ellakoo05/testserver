97b34d604894ec1e425cab3a7ed8a8f5fad0c9e0
ellakoo05 <ella@github.com>
2018-11-29 11:45:15 -0800

    Update index.js

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ app.get("/fuelhubdata", (req, resp)=>{
       var len = $('.styles__price___3DxO5', html).length,
           pr = $('.styles__price___3DxO5', html),
           addr = $('.styles__address___8IK98', html),
-          station = $('.styles__stationNameHeader___24lb3', html),
-          logo=$('.styles__logoImage___2zpSA', html)
+          station = $('.styles__stationNameHeader___24lb3', html)
+          // logo=$('.styles__logoImage___2zpSA', html)
           
           console.log(logo)
       //console.log(len);
@@ -44,7 +44,7 @@ app.get("/fuelhubdata", (req, resp)=>{
           price: pr[i].children[0].data,
           addr:addr[i].children[0].data,
           station:station[i].children[0].data,
-          logo:logo[i].children[0].data
+          // logo:logo[i].children[0].data
         });
       }
 
